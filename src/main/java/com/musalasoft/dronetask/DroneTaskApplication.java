811f9127f23da1a4c0e0d8127ffddd03b116754c
Stelan Briyan Simonsz <stelan@objectoneworld.com>
2022-11-29 14:30:07 +0530

    feat: initial commit

---
+++ src/main/java/com/musalasoft/dronetask/DroneTaskApplication.java
@@ -0,0 +1,13 @@
+package com.musalasoft.dronetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DroneTaskApplication.class, args);
+    }
+
+}
