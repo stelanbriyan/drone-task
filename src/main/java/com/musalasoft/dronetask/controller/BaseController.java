1ffa8f09ed9ea72443c375ccbe83078bd1890148
Stelan Briyan Simonsz <stelan@objectoneworld.com>
2022-12-02 18:54:38 +0530

    feat: add javax validation & catch input errors in handle exceptions.

--- src/main/java/com/musalasoft/dronetask/controller/BaseController.java
+++ src/main/java/com/musalasoft/dronetask/controller/BaseController.java
@@ -4,16 +4,28 @@ import com.musalasoft.dronetask.dto.ErrorResponseDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.List;
+import java.util.Map;
+
 @Slf4j
 public class BaseController {
 
-	@ExceptionHandler({ RuntimeException.class })
-	public final ResponseEntity<Object> handleExceptions(Exception e, WebRequest request) {
+	@ExceptionHandler({ RuntimeException.class, MethodArgumentNotValidException.class })
+	public final ResponseEntity<Object> handleExceptions(Exception e, WebRequest request, Errors errors) {
 		String path = ((ServletWebRequest) request).getRequest().getRequestURI();
+
+		if (errors.hasErrors()) {
+			List<Map<String, String>> validationError = errors.getFieldErrors().stream()
+					.map(er -> Map.of("field", er.getField(), "message", er.getDefaultMessage())).toList();
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(ErrorResponseDTO.builder().description(validationError).path(path).build());
+		}
 
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 				.body(ErrorResponseDTO.builder().description(e.getMessage()).path(path).build());
