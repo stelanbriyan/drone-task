7e986420b0d809e45da7e6d07811c53893b1a736
Stelan Briyan Simonsz <stelan@objectoneworld.com>
2022-12-02 15:44:54 +0530

    feat: upload image to S3

--- src/main/java/com/musalasoft/dronetask/infrastructure/adapter/S3Adapter.java
+++ src/main/java/com/musalasoft/dronetask/infrastructure/adapter/S3Adapter.java
@@ -1,13 +1,35 @@
 package com.musalasoft.dronetask.infrastructure.adapter;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Component
 public class S3Adapter {
 
+	private static final String BUCKET_NAME = "drone-task";
+
+	private final AmazonS3 amazonS3;
+
+	public S3Adapter(AmazonS3 amazonS3) {
+		this.amazonS3 = amazonS3;
+	}
+
 	public String upload(MultipartFile file) {
-		return null;
+		try {
+			String folderName = "/medication/";
+			amazonS3.putObject(new PutObjectRequest(BUCKET_NAME.concat(folderName), file.getOriginalFilename(),
+					file.getInputStream(), null));
+			return folderName.concat(file.getOriginalFilename());
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		return Strings.EMPTY;
 	}
 
 }
